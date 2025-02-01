41aaafd07aa31db499a6a1a9eeb700649ef1a327
orut34iop <h64ut2qg5lou013@gmail.com>
2025-02-01 09:39:33 +0800

    添加到右键菜单

--- background.js
+++ background.js
@@ -1,11 +1,48 @@
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.contextMenus.create({
+    id: 'processText',
+    title: '查询Emby媒体库',
+    contexts: ['selection']
+  });
+});
+
+chrome.contextMenus.onClicked.addListener((info, tab) => {
+  if (info.menuItemId === 'processText') {
+    chrome.scripting.executeScript({
+      target: { tabId: tab.id },
+      function: () => {
+        // 获取选中文本并处理
+        const selectedText = window.getSelection().toString().trim();
+        if (selectedText) {
+          fetch('http://localhost:5000/process', {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ text: selectedText })
+          })
+          .then(response => response.json())
+          .then(data => {
+            alert(data.result);
+          })
+          .catch(error => {
+            alert('错误: ' + error.message);
+          });
+        } else {
+          alert('请先选择影剧名称!');
+        }
+      }
+    });
+  }
+});
+
 chrome.action.onClicked.addListener((tab) => {
   chrome.scripting.executeScript({
     target: { tabId: tab.id },
     function: () => {
-      // Get the selected text
+      // 获取选中文本并处理
       const selectedText = window.getSelection().toString().trim();
       if (selectedText) {
-        // Send selected text to our local Python server
         fetch('http://localhost:5000/process', {
           method: 'POST',
           headers: {
@@ -15,15 +52,14 @@ chrome.action.onClicked.addListener((tab) => {
         })
         .then(response => response.json())
         .then(data => {
-          // Show result in an alert
-            alert(data.result);
-          })
-          .catch(error => {
-            alert('错误: ' + error.message);
-          });
-        } else {
-          alert('请先选择影剧名!');
-        }
+          alert(data.result);
+        })
+        .catch(error => {
+          alert('错误: ' + error.message);
+        });
+      } else {
+        alert('请先选择影剧名称!');
+      }
     }
   });
 });
